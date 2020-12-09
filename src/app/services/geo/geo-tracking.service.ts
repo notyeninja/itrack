d124b64d101c2623745185c0abfac8058230c965
Milind Wankhede <milindwankhede@Milinds-MacBook-Pro-2.local>
2020-12-08 19:37:01 -0800

    updated config

--- src/app/services/geo/geo-tracking.service.ts
+++ src/app/services/geo/geo-tracking.service.ts
@@ -16,16 +16,16 @@ export class GeoTrackingService {
   private config: BackgroundGeolocationConfig = {
     locationProvider: BackgroundGeolocationLocationProvider.RAW_PROVIDER,
     desiredAccuracy: 10,
-    stationaryRadius: 50,
-    distanceFilter: 300,
+    stationaryRadius: 10,
+    distanceFilter: 10,
     notificationTitle: "iNative Running",
     notificationText: "Keeping track of things.",
-    debug: false,
+    debug: true,
     interval: 5000,
-    fastestInterval: 15000,
+    fastestInterval: 10000,
     activitiesInterval: 5000,
     stopOnTerminate: true,
-    // stopOnStillActivity: false,
+    stopOnStillActivity: false,
   };
 
   constructor(
